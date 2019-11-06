74c44bcc1e3c83d68e3b633b439a69d007fad3fc
Rory Murray <rory.christian.murray@gmail.com>
2019-11-06 08:05:03 -0500

    Merge_sort working

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,5 +1,6 @@
 data1 = [1, 2, 3, 4, 5, 6, 7, 8]
 data2 = [100, 101, 102, 103, 104, 105, 106, 107]
+data3 = [37, 42, 67, 1, 9, 3, 10, 4, 12]
 
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 
@@ -42,11 +43,35 @@ print(mester1)
 
 def merge_sort(arr):
     # TO-DO
+    # we have a helper function that merges sorted arrays
+    # we know that an array of len = 1 is sorted
+    # therefore, we need to split the array coming in
+    # recursively until it is len = 1
+    # and then run our merge() helper function
+
+    # 1. while loop that checks if len(arr) > 1
+    # 2. if (1) is true, split arr in half to lhs and rhs arrays
+    # 3. if len(arr) = 1, then run merge on it (with another sorted array)
+
+    # instantiate lhs and rhs arrays
+    lhs = []
+    rhs = []
+
+    if len(arr) > 1:
+        print(f"arr length = {len(arr)}")
+        lhs = merge_sort(merge_sort(arr[: len(arr) // 2]))
+        rhs = merge_sort(merge_sort(arr[len(arr) // 2:]))
+        arr = merge(lhs, rhs)
+        print(f"arr : {arr}")
 
     return arr
 
 
+merge_sort_test_1 = merge_sort(data3)
+
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
